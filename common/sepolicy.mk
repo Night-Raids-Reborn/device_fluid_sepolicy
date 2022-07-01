LineageOS/android_device_lineage_sepolicy

atv: Allow setupwraith configs to be set by vendor
Change-Id: I0e2831019d5bbb9d432bbad48cd91c98f3c3cb6f

common/sepolicy.mk
@@ -33,3 +33,8 @@ endif
 
 # Selectively include legacy rules defined by the products
 -include device/lineage/sepolicy/legacy-common/sepolicy.mk
+
+# Include atv rules on atv product
+ifeq ($(PRODUCT_IS_ATV), true)
+include device/lineage/sepolicy/atv/sepolicy.mk
+endif

Night-Raids-Reborn/device_fluid_sepolicy

(cherry picked from commit 72049da6bc559ad5e57a2824b975fba5102258a0)

common/sepolicy.mk
@@ -27,3 +27,8 @@ endif
 
 # Selectively include legacy rules defined by the products
 -include device/fluid/sepolicy/legacy-common/sepolicy.mk
+
+# Include atv rules on atv product
+ifeq ($(PRODUCT_IS_ATV), true)
+include device/fluid/sepolicy/atv/sepolicy.mk
+endif